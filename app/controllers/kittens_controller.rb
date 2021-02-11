f12f339bd12f91bbeb088759caa219e6edeba986
steven <steven.h@live.com>
2021-02-10 19:00:08 -0800

    fill in rest of RESTful actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,24 +1,50 @@
 class KittensController < ApplicationController
   def index
-    @kittens=Kitten.all
+    @kittens = Kitten.all
   end
 
   def show
-    @kitten=Kitten.find(params[:id])
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "#{@kitten.name} has been successfully saved!"
+      redirect_to(kittens_path)
+    else
+      render(:new)
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:success] = "#{@kitten.name} has been successfully updated!"
+      redirect_to(kittens_path)
+    else
+      render(:edit)
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "#{@kitten.name} has been deleted :("
+    redirect_to(kittens_path)
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
